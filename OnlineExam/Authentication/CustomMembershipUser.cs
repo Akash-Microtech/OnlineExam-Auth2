ce5ec8c072ad88c55335391de9d5bb2c346be506
Akash-Microtech <akashpraj31@gmail.com>
2021-06-23 16:50:05 +0530

    Add project files.

---
+++ OnlineExam/Authentication/CustomMembershipUser.cs
@@ -0,0 +1,29 @@
+﻿using OnlineExam.DbContext;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+
+namespace OnlineExam.Authentication
+{
+    public class CustomMembershipUser : MembershipUser
+    {
+        #region User Properties  
+
+        public int UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public UserRole Role { get; set; }
+
+        #endregion
+
+        public CustomMembershipUser(User user) : base("CustomMembership", user.UserName, user.Id, user.Email, string.Empty, string.Empty, true, false, DateTime.Now, DateTime.Now, DateTime.Now, DateTime.Now, DateTime.Now)
+        {
+            UserId = user.Id;
+            FirstName = user.FirstName;
+            LastName = user.LastName;
+            Role = user.UserRole;
+        }
+    }
+}
---
+++ OnlineExam/Global.asax.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using System.Web.Security;
+using System.Web.SessionState;
+using System.Web.Http;
+using Newtonsoft.Json;
+using OnlineExam.Authentication;
+using OnlineExam.Models;
+
+namespace OnlineExam
+{
+    public class Global : HttpApplication
+    {
+        void Application_Start(object sender, EventArgs e)
+        {
+            // Code that runs on application startup
+            AreaRegistration.RegisterAllAreas();
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);            
+        }
+
+        protected void Application_PostAuthenticateRequest(Object sender, EventArgs e)
+        {
+            HttpCookie authCookie = Request.Cookies["Cookie1"];
+            if (authCookie != null)
+            {
+                FormsAuthenticationTicket authTicket = FormsAuthentication.Decrypt(authCookie.Value);
+
+                var serializeModel = JsonConvert.DeserializeObject<CustomSerializeModel>(authTicket.UserData);
+
+                CustomPrincipal principal = new CustomPrincipal(authTicket.Name);
+
+                principal.UserId = serializeModel.UserId;
+                principal.FirstName = serializeModel.FirstName;
+                principal.LastName = serializeModel.LastName;
+                principal.Role = serializeModel.RoleName;
+
+                HttpContext.Current.User = principal;
+            }
+
+        }
+    }
+}
